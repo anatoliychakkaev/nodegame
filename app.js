49a3c53a87b03560e8b2d5ace2cd2499f8183faa
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2010-11-16 22:19:05 +0300

    Misc amends

--- app.js
+++ app.js
@@ -3,65 +3,83 @@
  * Module dependencies.
  */
 
-var express = require('express');
-
-var app = module.exports = express.createServer(),
+var express = require('express'),
+    app = module.exports = express.createServer(),
     RedisStore = require('connect-redis'),
-    models = require('./lib/models.js');
+    store = new RedisStore,
+    facebooker = require('./lib/facebooker.js'),
+    io = require('socket.io');
+
+var session_key = 'connect.sidw';
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.use(express.cookieDecoder());
-  app.use(express.session({ store: new RedisStore }));
-  app.use(express.bodyDecoder());
-  app.use(express.methodOverride());
-  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-  app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+    app.use(express.staticProvider(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.use(express.cookieDecoder());
+    app.use(express.session({ store: store, key: session_key }));
+    app.use(facebooker.connect);
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler());
 });
+
+// Controller
+
+var c = require('./lib/controller.js');
 
 // Routes
 
-var loadUser = function (req, res, next) {
-    if (!req.session.player_id) {
-        models.Player.create(function () {
-            req.session.player_id = this.id;
-            global.user = this;
-            this.loadGame('reversi', next);
-        });
-    } else {
-        models.Player.find(req.session.player_id, function (err) {
-            if (!err) {
-                global.user = this;
-                this.loadGame('reversi', next);
-            } else {
-                next(new Error('Could not load user with id ' + req.session.player_id));
-            }
-        });
-    }
-};
-
-app.get('/', loadUser, function(req, res){
-  res.render('index.jade', {
-    locals: {
-        title: 'Express'
-    }
-  });
-});
+app.get('/',     facebooker.check_connection, c.loadUser, c.index);
+//app.get('/wait', c.loadUser, c.wait);
+//app.get('/move', c.loadUser, c.move);
 
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port)
+    app.listen(parseInt(process.ARGV[2], 10) || 4321);
+    //var utils = require('connect/utils');
+    console.log("Express server listening on port %d", app.address().port)
+
+    var socket = io.listen(app);
+
+    socket.on('connection', function(client){
+        var player;
+        function authorize(session_hash) {
+            console.log('client connected');
+            store.get(session_hash, function (err, session) {
+                client.request.session = session;
+                c.loadUser(client.request, client, function () {
+                    player = client.request.player;
+                    if (player) {
+                        player.connect(client);
+                    } else {
+                        throw new Error('Player not defined');
+                    }
+                });
+            });
+        };
+        client.on('message', function(msg){
+            if (msg.action == 'authorize') {
+                authorize(msg.secret);
+            } else {
+                player.perform(msg);
+            }
+        });
+        client.on('disconnect', function(){
+            console.log('client ' + player.id + ' disconnected');
+            player.disconnect();
+        });
+    });
 }
+
---
+++ public/reversi-cli.js
@@ -0,0 +1,246 @@
+$.fn.reversi = function (player, current_color, board, drawer) {
+    return new window.exports.ReversiGame(player, current_color, board, drawer, this);
+};
+$(function () {
+    var undef;
+    var cell_size = 60;
+    var padding = 10;
+    var board_size = cell_size * 8 + padding * 2;
+    var board = $('#board canvas')[0];
+    var info = $('#board .info');
+    var game;
+    var CURRENT_POSITION;
+
+    board.width = board_size;
+    board.height = board_size;
+
+    function clear(r) {
+        context.fillStyle = '#ccb';
+        context.fillRect(-cell_size/2, -cell_size/2, cell_size, cell_size);
+    }
+
+    function circle(r, style) {
+        if (!style) {
+            style = '#bbb,#ddd';
+        }
+        style = style.split(',');
+        var relief = style[2] ? 1 : -1;
+        var grad = style[0].split('-');
+        if (grad.length > 1) {
+            style[0] = context.createRadialGradient(
+                relief * cell_size*.25, relief * cell_size*.25, cell_size*.05,
+                0, 0, cell_size*.5
+            );
+            style[0].addColorStop(0, grad[0]);
+            style[0].addColorStop(.6, grad[1]);
+            style[0].addColorStop(1, grad[2]);
+        }
+
+        context.beginPath();
+        context.arc(0, 0, r, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fillStyle = style[0];
+        context.fill();
+
+        context.strokeWeight = 1;
+        context.beginPath();
+        context.arc(0, 0, r, 0, Math.PI * 2, true);
+        context.globalAlpha = 0.5;
+        context.strokeStyle = style[1];
+        context.stroke();
+    }
+
+    var s = {
+        b: '#666-#222-#000,#000',
+        w: '#fff-#e3e3e3-#afafaf,#aaa',
+        0: '#c2c2b4-#c2c2b0-#b0b0a0,#bbb,-',
+        // 0: '#b4b4b4-#b0b0b0-#999,#bbb,-',
+        W: '#ccc-#ccc-#aaa,#ccc,-',
+        B: '#888-#888-#666,#888,-' };
+
+        function place_chip(i, j, c) {
+            context.save();
+            context.translate((j + 0.5) * cell_size, (i + 0.5) * cell_size);
+            clear();
+            circle(cell_size / 2 - 2, s[c]);
+            context.restore();
+        }
+
+        function initial_draw(position) {
+            if (OVER_CHIP) {
+                place_chip(OVER_CHIP.i, OVER_CHIP.j, '0');
+                OVER_CHIP = false;
+            }
+            CURRENT_POSITION = position;
+            for (var i = 0; i < 8; i++) {
+                for (var j = 0; j < 8; j++) {
+                    place_chip(i, j, position[i][j]);
+                }
+            }
+        }
+
+        function animate_revert(diff, callback, step, what) {
+            var steps = [.8, .5, .3, .1, 'r', .1, .3, .5, .8, 1];
+            if (step === undef) {
+                step = 0;
+            }
+            if (what === undef) {
+                what = 'from';
+            }
+            if (!steps[step]) {
+                callback();
+                return;
+            }
+            if (steps[step] === 'r') {
+                what = 'to';
+                step++;
+            }
+            for (var p in diff) {
+                context.save();
+                context.translate((diff[p].j + 0.5) * cell_size, (diff[p].i + 0.5) * cell_size);
+                circle(cell_size / 2 - 0, s['0']);
+                context.rotate(Math.PI*.25);
+                context.scale(steps[step], 1);
+                circle(cell_size / 2 - 2, s[diff[p][what]]);
+                context.restore();
+            }
+            setTimeout(function () {
+                animate_revert(diff, callback, step + 1, what);
+            }, 1000/25);
+        }
+
+        function clicked(x, y) {
+            if (!game.can_player_move()) return;
+            var i = Math.ceil((y - padding) / cell_size) - 1;
+            var j = Math.ceil((x - padding) / cell_size) - 1;
+
+            if (i < 0 || j < 0 || i > 7 || j > 7) {
+                return;
+            }
+
+            context.fillStyle = '#FFF';
+            OVER_CHIP.p = CURRENT_POSITION[i][j];
+            game.move(i + ':' + j);
+        }
+
+        var $board = $(board);
+        $(board).click(function (e) {
+            var offset = $board.offset();
+            clicked(e.clientX - offset.left + window.scrollX, e.clientY - offset.top + window.scrollY);
+        });
+
+        var OVER_CHIP = null;
+        function moved(x, y) {
+            if (!game.can_player_move()) return;
+            var i = Math.ceil((y - padding) / cell_size) - 1;
+            var j = Math.ceil((x - padding) / cell_size) - 1;
+            if (i < 0 || j < 0 || i > 7 || j > 7) {
+                return;
+            }
+
+            var p = CURRENT_POSITION[i][j];
+            if (OVER_CHIP && (OVER_CHIP.j != i || OVER_CHIP.i != j)) {
+                place_chip(OVER_CHIP.i, OVER_CHIP.j, OVER_CHIP.p);
+                OVER_CHIP = false;
+            }
+            if ((p == 'W' || p == 'B') && (!OVER_CHIP || (OVER_CHIP.i != i && OVER_CHIP.j != j))) {
+                OVER_CHIP = {i: i, j: j, p: p};
+                context.globalAlpha = 0.5;
+                place_chip(i, j, p.toLowerCase());
+                context.globalAlpha = 1;
+            }
+        }
+
+        $(board).mousemove(function (e) {
+            var offset = $board.offset();
+            moved(e.clientX - offset.left + window.scrollX, e.clientY - offset.top + window.scrollY);
+        });
+
+        var context = board.getContext('2d');
+        context.fillStyle = "#c4c4b3";
+        context.fillRect(0, 0, board_size, board_size);
+        context.translate(padding, padding);
+
+        function update_board(data) {
+            if (data.prev_position) {
+                var diff = [], from, to;
+                for (var i = 0; i < 8; i++) {
+                    for (var j = 0; j < 8; j++) {
+                        from = data.prev_position[i][j];
+                        to = data.position[i][j];
+                        if ((from == 'w' || from == 'b') && from != to) {
+                            diff.push({i:i,j:j,from:from,to:to});
+                        }
+                        if (from == 'W' || from == 'B') {
+                            if (to == 'b' || to == 'w') {
+                                place_chip(i, j, to);
+                            }
+                        }
+                    }
+                }
+                animate_revert(diff, function () {
+                    initial_draw(data.position);
+                });
+            } else {
+                initial_draw(data.position);
+            }
+        }
+
+        var res = BOARD;
+
+        s[res.player == 'w' ? 'B' : 'W'] = s['0'];
+        game = $('#board').reversi(res.player, res.color == 'b', res.position, update_board);
+        game.join();
+        game.after_move = function (coords) {
+            if (!game.pwned_by(res.player)) {
+                info.html('Opponent\'s turn.');
+            }
+            console.log('send to socket: move,', coords);
+            SOCKET.send({action: "move", coords: coords});
+        };
+        var SOCKET = new io.Socket(document.location.hostname);
+        SOCKET.connect();
+        SOCKET.send({action: 'authorize', secret: BOARD.secret});
+
+        if (res.state == 'move') {
+            game.join();
+            game.board.color = res.color == 'b';
+            info.html('Your turn');
+        } else if (res.action == 'end') {
+            info.html('Game finished');
+        } else if (res.action == 'wait') {
+            info.html('Opponents turn');
+        } else {
+            info.html('Awaiting for opponent connection');
+        }
+
+        SOCKET.on('message', function (msg) {
+            if (typeof msg == 'string') {
+                console.log(msg);
+                msg = JSON.parse(msg);
+            }
+            switch (msg.action) {
+                case 'opponent_connected':
+                    info.html('Opponent joined. You can move.');
+                    var $opp = $('#opponent_info');
+                    console.log($opp);
+                    $opp.find('.avatar img').attr('src', 'http://graph.facebook.com/' + msg.user.info.id + '/picture');
+                    $opp.find('.username').html(msg.user.info.name);
+                    game.join();
+                break;
+                case 'move':
+                    console.log('move to', msg.coords);
+                    info.html('Your turn.');
+                    game.join();
+                    game.move(msg.coords, true);
+                break;
+                case 'end':
+                info.html('End game. You ' + (
+                    msg.info.b > msg.info.w && res.player == 'b' ||
+                    msg.info.b < msg.info.w && res.player == 'w' ?
+                    'win' : 'lose'
+                ) + '.');
+                break;
+            }
+        });
+});
